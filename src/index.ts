2cc2dbb01021d9690c6348c5a02195a373bfeab5
Kmario19 <kmario1019@gmail.com>
2025-04-01 19:33:09 -0500

    feat: Add Mongoose for MongoDB connection and update dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,18 @@
 import dotenv from 'dotenv';
 import express from 'express';
+import mongoose from 'mongoose';
 
 dotenv.config();
 
 export const app = express();
 const PORT = +(process.env.PORT || '3000');
+const MONGODB_URI = process.env.MONGODB_URI as string;
+
+// MongoDB Connection
+mongoose
+  .connect(MONGODB_URI)
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('MongoDB connection error:', err));
 
 // Middleware to parse JSON
 app.use(express.json());
