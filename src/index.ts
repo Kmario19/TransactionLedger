6a1cc46b8927bfafcd21c6752a6d6b1a7055bf41
Kmario19 <kmario1019@gmail.com>
2025-04-03 12:06:42 -0500

    fix: update organizeImports configuration to ignore src/index.ts and reorder import statements in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
-import { connectDB } from '@/config/database';
 import loadEnvironment from '@/config/environment';
+import { connectDB } from '@/config/database';
 
 import app from './app';
 
