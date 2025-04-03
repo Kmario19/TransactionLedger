62ef37ee942fa83141622c764f7cba5a0631369b
Kmario19 <kmario1019@gmail.com>
2025-04-02 20:18:28 -0500

    feat: integrate Swagger UI for API documentation

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import apiRoutes from '@/routes';
 import errorHandler from '@/middleware/errorHandler';
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from './../swagger.json';
 
 const app = express();
 
@@ -9,6 +11,7 @@ app.use(express.json());
 
 // Routes
 app.use('/api', apiRoutes);
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // Error handling middleware
 app.use(errorHandler);
