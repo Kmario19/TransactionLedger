8a476f735f984cd99dbe9ce32dca9af355d78177
Kmario19 <kmario1019@gmail.com>
2025-04-03 15:39:49 -0500

    feat: add account creation conflict handling for duplicate names

--- src/routes/account/create/controller.ts
+++ src/routes/account/create/controller.ts
@@ -3,6 +3,14 @@ import { StatusCodes } from 'http-status-codes';
 import { Account } from '@/models/Account';
 
 export default async (req: Request, res: Response) => {
+  const existingAccount = await Account.findOne({ name: req.body.name });
+
+  if (existingAccount) {
+    return res.status(StatusCodes.CONFLICT).json({
+      message: 'Account with this name already exists',
+    });
+  }
+
   const account = await Account.create(req.body);
 
   res.status(StatusCodes.CREATED).json(account);
